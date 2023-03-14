40dd6cb27fa5ec9e43d0406d92ba5af3f9d5be15
etunk <96209418+etunk@users.noreply.github.com>
2023-03-14 16:37:31 -0700

    letsgooo

--- Exercises and Homework/Lesson 17/01_Bad_Intern_At_The_Horse_Farm/starter_code/js/exercise.js
+++ Exercises and Homework/Lesson 17/01_Bad_Intern_At_The_Horse_Farm/starter_code/js/exercise.js
@@ -1,23 +1,27 @@
-document.addEventListener('DUMContentLoaded', function(event) {
-    document.querySelectorAll(".breedContainer .tigger").forEach(function(element){
-        element.addEventListener("cluck",function(){
+document.addEventListener('DOMContentLoaded', function(event) {
+    console.log("Hi 1")
+    document.querySelectorAll(".breedContainer .toggler").forEach(function(element){
+        console.log("Hi 2")
+        console.log(this)
+        element.addEventListener("click",function(){
         this.parentElement.classList.toggle("breedContainerLG");
-        if(this.getAttribute('src') == "img/240px-Gnome-window-close.svg.png"){
-            this.setAttribute('src',"img/48px-Gnome-list-add.svg");
+        if(this.getAttribute('src') =="img/240px-Gnome-window-close.svg.png" ){
+            this.setAttribute('src',"img/48px-Gnome-list-add.svg.png");
         }else{
-            this.setAttribute('src',"img/240px-Gnome-window-close.svg.png"];
+            this.setAttribute('src',"img/240px-Gnome-window-close.svg.png");
         }
     });
 });
 
-document.querySelector(".breedType").addEventListener('change',function(){
-    var showType = this.val();
+document.querySelector("#breedType").addEventListener('change',function(){
+    var showType = this.value;
+        console.log(showType)
 
         // For each article tag in the class breedsOuterContainer
-        document.querySelector(".breedsOuterContainer article").forEach(function(element){
+        document.querySelectorAll(".breedsOuterContainer article").forEach(function(elements){
 
             // Check to see if we are showing all and/or the article has the desired class
-            if[elements.classList.contains(showType) || showType == "all"]{
+            if(elements.classList.contains(showType) || showType == "all"){
                 // Article has the class
                 elements.style.display = 'inline-block';
             }else{
